c7f57e1de7852c8ce909448ff99ea0e075a0db00
jihtin <129176693+jihtin@users.noreply.github.com>
2023-09-20 14:34:00 +0530

    updates

--- Rentcar/Rentcar/settings.py
+++ Rentcar/Rentcar/settings.py
@@ -17,13 +17,13 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SECRET_KEY = 'django-insecure-{SECRETKEY}'
 SECRETKEY=os.getenv('SECRETKEY')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-{SECRETKEY}'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -44,16 +44,11 @@ INSTALLED_APPS = [
     'accounts',
     'Admin_panel',
     'Booking',
-    'crispy_forms',
-    'crispy_bootstrap5',
     'fontawesomefree'
    
     
 ]
 
-CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
-
-CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 
 MIDDLEWARE = [
